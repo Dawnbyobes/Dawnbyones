5dcd95f8ef7d269dc87537f158753f5931979b35
Dawnbyobes <ryinnoeht@gmail.com>
2026-06-25 11:18:45 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Novels from "./pages/Novels";
+import NovelDetail from "./pages/NovelDetail";
+import Read from "./pages/Read";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/novels" element={<Novels />} />
+        <Route path="/novel/:slug" element={<NovelDetail />} />
+        <Route path="/read/:chapterId" element={<Read />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  );
+}
